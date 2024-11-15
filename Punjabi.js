059ef46e42d0dc5c4a28fb7777257060ecb14baa
harcharan-m <harcharansingh82646@gmail.com>
2024-11-15 19:06:44 +0530

    Add files via upload

---
+++ Punjabi.js
@@ -0,0 +1,40 @@
+// PDF path and video link functions
+function openOnlinePDF() {
+    window.open('https://drive.google.com/file/d/1A4MU6ZgWenD_fCtpJFL6XINC_oAJTAWV/view', '_blank');
+  }
+  
+  function downloadPDF() {
+    window.open('PunjabiNovel.pdf','_blank');
+  }
+  
+  function watchVideo() {
+    window.open('https://youtu.be/2NBTPoNV4sY?si=gWD8mKiz7NnsDPvK', '_blank');
+  }
+  
+  // Long Q/A functions
+  function openLongQAOnline() {
+    window.open('https://drive.google.com/file/d/1phs7qRH7NTvh5fLsJ9lHnMBZ1WoMJ9Xn/view', '_blank');
+  }
+  
+  function downloadLongQA() {
+    window.open('LongQA.pdf');
+  }
+  
+  // Short Q/A functions
+  function openShortQAOnline() {
+    window.open('https://drive.google.com/file/d/15Yp874rPsVAnA-2A0AcggdFCEuUDf8Sb/view', '_blank');
+  }
+  
+  function downloadShortQA() {
+    window.open('ShortQA.pdf');
+  }
+  
+  // Novel Related Q/A functions
+  function openNovelQAOnline() {
+    window.open('https://drive.google.com/file/d/1vJPLFu6smiltKpJxACjD7YkbirvYPBgC/view', '_blank');
+  }
+  
+  function downloadNovelQA() {
+    window.open("NovelrelatedQA.pdf");
+  }
+  
